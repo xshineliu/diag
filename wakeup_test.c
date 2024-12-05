9241996a7a3f987b014f7e36d08458efed2beb67
Shine Liu <shinel@foxmail.com>
2024-12-05 10:17:29 +0800

    Update wakeup_test.c

--- wakeup_test.c
+++ wakeup_test.c
@@ -1,42 +1,43 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <time.h>
 #include <unistd.h>
 
-#include <time.h>
-
-#include <stdio.h>
-
-#include <stdint.h>
-
-int main() {
-
+int main () {
   time_t t;
   struct timespec start, end;
   uint64_t delta_us;
-  struct tm * tm;
+  struct tm *tm;
   int i = 0;
   int last = -1;
-  int delta = -1;
+  // int delta = -1;
+  struct rusage usage;
 
-  clock_gettime(CLOCK_MONOTONIC_RAW, & start);
+  clock_gettime (CLOCK_MONOTONIC_RAW, &start);
 
-  t = time(NULL);
-  last = (int) t;
-  while (i < 6000) {
-    ++i;
-    usleep(100000);
-    if (i % 10 == 0) {
-      t = time(NULL);
-      tm = localtime( & t);
-      delta = (int) t - last;
-      last = (int) t;
-      if (delta != 1) {
-        printf("now: %03d at %02d:%02d:%02d *** %d\n", i / 10, tm - > tm_hour, tm - > tm_min, tm - > tm_sec, delta);
-      } else {
-        printf("now: %03d at %02d:%02d:%02d\n", i / 10, tm - > tm_hour, tm - > tm_min, tm - > tm_sec);
-      }
+  t = time (NULL);
+  last = (int)t;
+  while (i < 600 * 1000) {
+      ++i;
+      usleep (10 * 1000);
+      if (i % 100 == 0) {
+          t = time (NULL);
+          tm = localtime (&t);
+          clock_gettime (CLOCK_MONOTONIC_RAW, &end);
+          delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+          memcpy (&start, &end, sizeof (struct timespec));
+          getrusage (RUSAGE_SELF, &usage);
+          if (delta_us > 1100000) {
+              printf ("now: %03d at %02d:%02d:%02d %9lld %9lld *** %lld\n", i / 100, tm->tm_hour, tm->tm_min, tm->tm_sec, usage.ru_nvcsw, usage.ru_nivcsw, delta_us);
+          } else {
+              printf ("now: %03d at %02d:%02d:%02d %9lld %9lld\n", i / 100, tm->tm_hour, tm->tm_min, tm->tm_sec, usage.ru_nvcsw, usage.ru_nivcsw);
+          }
     }
   }
-  clock_gettime(CLOCK_MONOTONIC_RAW, & end);
+  clock_gettime (CLOCK_MONOTONIC_RAW, &end);
   delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
-  printf("Taken %lld\n", delta_us);
+  printf ("Taken %lld\n", delta_us);
   return 0;
 }
